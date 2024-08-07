4e9b4c35336d0b111cacdafe82650e38d408d56b
EoRedneVida <joaoguilhermempdev@gmail.com>
2024-08-07 08:44:05 -0300

    Inicio do aprendizado

---
+++ exercicios/exercicio01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello pucpr\n");
+    double pi = 3.14;
+    double raio = 10.0;
+    double area = pi * raio * raio;
+    printf("%f\n", area);
+    return 0;
+}
